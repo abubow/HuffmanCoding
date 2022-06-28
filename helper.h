f0d481d41d3200a7fef1e9a828d3cbe91eb83d6b
smainc <40357673+smainc@users.noreply.github.com>
2022-06-28 19:18:13 +0500

    Added insert and delete

--- helper.h
+++ helper.h
@@ -21,24 +21,27 @@ struct Node {
 	bool operator<(Node& obj) {
 		return second < obj.second;
 	}
+	bool operator<=(Node& obj) {
+		return second <= obj.second;
+	}
 };
-std::vector<Node> wordCount(std::vector<char> chars){
-	std::vector<Node> counts;
+std::vector<Node*> wordCount(std::vector<char> chars){
+	std::vector<Node*> counts;
 	bool alreadyExists;
 	for(auto i:chars){
 		alreadyExists=false;
 		for(auto& it:counts){
-			if(i==it.first){
-				it.second=it.second + 1;
+			if(i==it->first){
+				it->second+= 1;
 				alreadyExists=true;
 				break;
 			}
 		}
 		if(alreadyExists == false)
 		{
-			Node res;
-			res.first = i;
-			res.second = 1;
+			Node* res = new Node;
+			res->first = i;
+			res->second = 1;
 			counts.push_back(res);
 		}	
         }
@@ -50,24 +53,54 @@ void swapV(type& a, type& b) {
 	a = b;
 	b = temp;
 }
-void insertInHeap(std::vector<Node>& vec, Node a) {
+void insertInHeap(std::vector<Node*>& vec, Node* a) {
 	vec.push_back(a);
-	for (int i = vec.size(); i > 0; i--)
+	for (int i = vec.size() - 1; i > 0;)
 	{
-
+		if (vec[i / 2] > vec[i]) {
+			swap(vec[i / 2], vec[i]);
+			i = (i / 2);
+		}
+		else
+			break;
 	}
 }
-Node popFromHeap(std::vector<Node> vec) {
-
+Node* popFromHeap(std::vector<Node*> vec) {
+	if (!(vec.size() > 1))
+		return nullptr;
+	Node* value = vec[0];
+	vec[0] = vec[vec.size() - 1];
+	vec.pop_back();
+	for (int i = 0; i < (vec.size() - 1) / 2;)
+	{
+		int left = 2 * i + 1;
+		int right = 2 * i + 2;
+		if (vec[i] > vec[left] && vec[left] <= vec[right]) {
+			swapV(vec[i], vec[left]);
+			i = left;
+		}
+		else if (vec[i] > vec[right] && vec[right] <= vec[2 * i + 1]) {
+			swapV(vec[i], vec[right]);
+			i = right;
+		}
+		else break;
+	}
+	return value;
 }
-Node createHuffmanTree(std::vector<Node> counts) {
+Node* createHuffmanTree(std::vector<Node*> counts) {
 	std::sort(counts.begin(), counts.end(),
 			[](auto& first, auto& second) {
 				return first < second;
 			});
-	for (int i = 0; i < counts.size(); i++)
+	int s = counts.size();
+	for (int i = 0; i < s-3; i++)
 	{
-
+		Node* top1 = popFromHeap(counts);
+		Node* top2 = popFromHeap(counts);
+		Node* collective = new Node();
+		collective->left = top1;
+		collective->right = top2;
+		insertInHeap(counts, collective);
 	}
 }
 std::vector <Node> createTable(std::vector <Node>& counts) {
